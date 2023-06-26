133ae8b661cf5266e2faf156d5e1c63d99bdaef5
Kevin Schans <kevinvdsd@hotmail.com>
2023-06-26 18:38:50 +0200

    Add app.ts

---
+++ src/app.ts
@@ -0,0 +1,22 @@
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import createDebug from "debug";
+const debug = createDebug("W9: App");
+
+export const app = express();
+
+debug("Loaded express App...");
+
+const corsOptions = {
+  origin: '*',
+};
+
+app.use(morgan('dev'));
+app.use(cors(corsOptions));
+app.use(express.json());
+
+app.use(express.static('public'));
+app.get('/', (_req, resp) => {
+  resp.send(`<h1>Alex & Melanie</h1>`)
+});
