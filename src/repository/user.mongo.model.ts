74af8ecb568194fe46db43b5d2b7e46fb7535677
Kevin Schans <kevinvdsd@hotmail.com>
2023-06-27 09:27:51 +0200

    Add repository and auth test

---
+++ src/repository/user.mongo.model.ts
@@ -0,0 +1,50 @@
+import { Schema, model } from 'mongoose';
+import { User } from '../entities/user.js';
+
+const userSchema = new Schema<User>({
+  id: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  role: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  userName: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  avatar: {
+    type: {
+      urlOriginal: { type: String },
+      url: { type: String },
+      mimetype: { type: String },
+      size: { type: Number },
+    },
+    required: true,
+    unique: true,
+  },
+});
+
+userSchema.set('toJSON', {
+  transform(_document, returnedObject) {
+    returnedObject.id = returnedObject._id;
+    delete returnedObject.__v;
+    delete returnedObject._id;
+    delete returnedObject.passwd;
+  },
+});
+
+export const UserModel = model('User', userSchema, 'users');
