b51ccdd8d594f3662d220b8a157d98b4e2bedcf7
Kevin Schans <kevinvdsd@hotmail.com>
2023-10-09 19:47:56 +0200

    Change server status

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,9 @@
 import http from 'http';
-import * as dotenv from 'dotenv';
 import { app } from './app.js';
-import createDebug from 'debug';
 import { dbConnect } from './db/db.connect.js';
+import createDebug from 'debug';
 const debug = createDebug('PF');
 
-dotenv.config();
 const PORT = process.env.PORT || 7777;
 
 const server = http.createServer(app);
@@ -13,33 +11,12 @@ const server = http.createServer(app);
 dbConnect()
   .then((mongoose) => {
     server.listen(PORT);
-    debug('Connected to db:', mongoose.connection.db.databaseName);
+    debug('Conected to db:', mongoose.connection.db.databaseName);
   })
   .catch((error) => {
     server.emit('error', error);
   });
 
 server.on('listening', () => {
-  const addressInfo = server.address();
-  if (addressInfo === null) {
-    server.emit('error', new Error('Invalid network address'));
-    return;
-  }
-
-  let bind: string;
-  if (typeof addressInfo === 'string') {
-    bind = 'pipe ' + addressInfo;
-  } else {
-    bind =
-      addressInfo.address === '::'
-        ? `http://localhost:${addressInfo?.port}`
-        : `port ${addressInfo?.port}`;
-  }
-
-  debug('Listening');
-  console.log(`Listening on ${bind}`);
-});
-
-server.on('error', (error) => {
-  console.log(`Error ${error.message}`);
+  debug('Listening on port ' + PORT);
 });
